d1b835ee4d4a8cab54953904112d832aee9e160a
zLaine <rumtehk_uh@yahoo.com>
2015-04-05 22:20:35 -0400

    Update PlayState.js

--- js/PlayState.js
+++ js/PlayState.js
@@ -8,11 +8,30 @@ PlayState.prototype =
     {
         console.log("Play");
         
-        
+        this.bubbles = this.game.add.group();
+        this.bubbles.enableBody = true;
+        this.bubbles.inputEnabled;
+        this.createBubbles(5);
     },
     
     update: function()
     {
         
+    }
+    
+    createBubbles: function(n)
+    {
+        for(var i = 0; i < n; i++)
+        {
+            var x = this.game.rnd.integerInRange(100, 770);
+            var y = this.game.rnd.integerInRange(0, 570);
+            var bubbleColor = this.game.rnd.integerInRange(0,7);
+            
+            var bubble = this.bubbles.create(x,y, "bubbles", bubbleColor);
+            
+            bubble.body.collideWorldBounds = true;
+            bubble.body.immovable = true;
+            
+        }
     }
 };
